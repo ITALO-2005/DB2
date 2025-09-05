57057f45380d56cf53c9227c77b394a4b2a4c875
ITALO-2005 <italo-santos.is@academico.ifpb.edu.br>
2025-09-05 17:09:21 +0000

    .

--- model.py
+++ model.py
@@ -3,7 +3,11 @@ import persistent
 class Autor(persistent.Persistent):
     def __init__(self, nome):
         self.nome = nome
-        self.livros = [] # Atributo para o relacionamento
+        self.__livros = [] # Atributo para o relacionamento
+
+    def adicionar_livro(self, livro):
+        self.__livros.append(livro)
+        self._p_changed = True
 
 class Livro(persistent.Persistent):
     def __init__(self, titulo, ano):
